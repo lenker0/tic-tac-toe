7fcfbc8562bd0eb3c73e53870e84a88fc2d35510
Michael <43700151+lenker0@users.noreply.github.com>
2019-05-17 20:35:19 +0300

    Add files via upload

---
+++ WindowsFormsApp2/Form1.Designer.cs
@@ -0,0 +1,170 @@
+﻿namespace WindowsFormsApp2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(1, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(600, 600);
+            this.panel1.TabIndex = 0;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            this.panel1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseClick);
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(630, 61);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(81, 17);
+            this.radioButton1.TabIndex = 1;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Крестиком";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.Click += new System.EventHandler(this.radioButton1_Click);
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(630, 93);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(71, 17);
+            this.radioButton2.TabIndex = 2;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Ноликом";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            this.radioButton2.Click += new System.EventHandler(this.radioButton2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(633, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(82, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Первый ход за";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(627, 137);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(77, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Сейчас ходит ";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(622, 444);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(102, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Побед Крестика: 0";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(629, 553);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Refresh map";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(622, 470);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(92, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Побед Нолика: 0";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(629, 524);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 8;
+            this.button2.Text = "Refresh txt";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(750, 601);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.panel1);
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(766, 640);
+            this.MinimumSize = new System.Drawing.Size(766, 640);
+            this.Name = "Form1";
+            this.RightToLeftLayout = true;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.RadioButton radioButton1;
+    }
+}
+
---
+++ WindowsFormsApp2/Form1.cs
@@ -0,0 +1,192 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace WindowsFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        int[,] mas = new int[3, 3] { { 2, 2, 2 }, { 2, 2, 2 }, { 2, 2, 2 } };
+        int x, y, i = 0, j = 0;
+        int px, py, x1, y1, x2, y2;
+        bool hod = false; bool smena = true;
+
+
+        public Form1()
+        {
+            InitializeComponent();            
+        }
+
+        private void Draw() // Рисование кр. 0
+        {
+            Graphics g = panel1.CreateGraphics();
+            
+            for (int i = 0; i < 3; i++)
+                for (int j = 0; j < 3; j++)
+                {
+                    if (mas[i, j] == 0) g.DrawEllipse(new Pen(Color.Red, 5), (j + 1) * 200 - 140, (i + 1) * 200 - 140, 80, 80);
+                    else if (mas[i, j] == 1)
+                    {
+                        g.DrawLine(new Pen(Color.Blue, 5), (j + 1) * 200 - 140, (i + 1) * 200 - 140, (j + 1) * 200 - 60, (i + 1) * 200 - 60);
+                        g.DrawLine(new Pen(Color.Blue, 5), (j + 1) * 200 - 60, (i + 1) * 200 - 140, (j + 1) * 200 - 140, (i + 1) * 200 - 60);
+                    }
+                }            
+        }
+   
+        private void radioButton1_Click(object sender, EventArgs e) // x ходит 1
+        {            
+            if (smena)
+            {
+                hod = false; smena = true;
+                label2.Text = "Сейчас ходит X";
+               
+            }
+
+        }
+
+        private void radioButton2_Click(object sender, EventArgs e) // 0 ходит 1
+        {
+            if (smena)
+            {
+                hod = true; smena = false;
+                label2.Text = "Сейчас ходит 0";
+            }
+        }       
+
+        private void panel1_Paint(object sender, PaintEventArgs e) // Границы карты 3х3 
+        {
+            Graphics g = e.Graphics;
+            g.DrawLine(new Pen(Color.Black,3), 200,0,200,600);
+            g.DrawLine(new Pen(Color.Black, 3), 400, 0, 400, 600);
+            g.DrawLine(new Pen(Color.Black, 3), 0, 200, 600, 200);
+            g.DrawLine(new Pen(Color.Black, 3), 0, 400, 600, 400);            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            refresh();
+        }
+
+        private void panel1_MouseClick(object sender, MouseEventArgs e)  //нажатие
+        {
+            x = e.Location.X;
+            y = e.Location.Y;
+            px = (int)x / 200;
+            py = (int)y / 200;
+            if (hod && (mas[py, px] == 2))
+            {
+                mas[py, px] = 0;
+                hod = !hod;
+                label2.Text = "Сейчас ходит X";
+            }
+            else if (mas[py, px] == 2) 
+            {
+                mas[py, px] = 1;
+                hod = !hod;
+                label2.Text = "Сейчас ходит 0";
+            }
+                          
+            Draw();
+            update_txt(py, px);
+            checkwin();
+            beat();
+        }
+
+        private void button2_Click(object sender, EventArgs e) // o4istka txt
+        {
+            File.WriteAllText("D:\\TTT.txt", string.Empty); 
+        }
+
+        void update_txt(int py, int px) // запись в блокнот (LOG)
+        {
+            string s;
+            StreamWriter sw = File.AppendText("D:\\test.txt");
+            if (hod)
+            {
+                s = "\nПервый игрок поставил крестик в клетку " + py + " " + px;
+            }
+            else
+            {
+                s = "\nВторой игрок поставил нолик в клетку " + py + " " + px;
+            }
+            sw.WriteLine(s);
+            sw.Flush();
+            sw.Close();
+        }
+
+        void checkwin() // check win 
+        {
+            
+            bool check = false;
+            if (mas[0, 0] == mas[0, 1] && mas[0, 1] == mas[0, 2] && mas[0, 2] != 2) { check = true; y1 = 100; x1 = 100; y2 = 100; x2 = 500; }
+            if (mas[1, 0] == mas[1, 1] && mas[1, 1] == mas[1, 2] && mas[1, 2] != 2) { check = true; y1 = 300; x1 = 100; y2 = 300; x2 = 500; }
+            if (mas[2, 0] == mas[2, 1] && mas[2, 1] == mas[2, 2] && mas[2, 2] != 2) { check = true; y1 = 500; x1 = 100; y2 = 500; x2 = 500; }
+            if (mas[0, 0] == mas[1, 0] && mas[1, 0] == mas[2, 0] && mas[2, 0] != 2) { check = true; y1 = 100; x1 = 100; y2 = 500; x2 = 100; }
+            if (mas[0, 1] == mas[1, 1] && mas[1, 1] == mas[2, 1] && mas[2, 1] != 2) { check = true; y1 = 100; x1 = 300; y2 = 500; x2 = 300; }
+            if (mas[0, 2] == mas[1, 2] && mas[1, 2] == mas[2, 2] && mas[2, 2] != 2) { check = true; y1 = 100; x1 = 500; y2 = 500; x2 = 500; }
+            if (mas[0, 0] == mas[1, 1] && mas[1, 1] == mas[2, 2] && mas[2, 2] != 2) { check = true; y1 = 100; x1 = 100; y2 = 500; x2 = 500; }
+            if (mas[0, 2] == mas[1, 1] && mas[1, 1] == mas[2, 0] && mas[2, 0] != 2) { check = true; y1 = 100; x1 = 500; y2 = 500; x2 = 100; }
+            if (check)
+            {
+                Graphics g = panel1.CreateGraphics();
+                string s; 
+                if (hod)
+                {
+                    g.DrawLine(new Pen(Color.Black, 3), x1, y1, x2, y2);
+                    MessageBox.Show("Победили крестики"); s = "Победили крестики"; i++; label3.Text = "Побед Крестика: " + i;
+                }
+                else
+                {
+                    g.DrawLine(new Pen(Color.Black, 3), x1, y1, x2, y2);
+                    MessageBox.Show("Победили нолики"); s = "Победили нолики"; j++;  label4.Text = "Побед Нолика: " + j;
+                }
+                StreamWriter sw = File.AppendText("D:\\TTT.txt");
+                sw.WriteLine(s);
+                sw.Flush();
+                sw.Close();
+                refresh();
+                smena = true;
+            }
+        }
+
+        void refresh() // refresh mapbI
+        {
+            panel1.Refresh(); hod = false;
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    mas[i, j] = 2;
+                }
+            }
+        }
+
+        void beat() // проверка на ничью
+        {
+            bool check = true;
+            for (int i = 0; i < 3; i++)
+                for (int j = 0; j < 3; j++)
+                {
+                    if (mas[i, j] == 2) check = false;
+                }
+            if (check)
+            {                 
+                string s;
+                MessageBox.Show("Ничья!"); s = "Ничья!";
+                StreamWriter sw = File.AppendText("D:\\TTT.txt");
+                sw.WriteLine(s);
+                sw.Flush();
+                sw.Close();
+                refresh();
+                smena = true;
+            }
+        }
+    }
+}
